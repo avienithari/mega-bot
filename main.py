4a3b36cbc2f9fe876689f89192a8d1ed4f61ba54
Maciej Borkowski <mmborkowski11@gmail.com>
2024-02-05 13:21:34 +0100

    feat: mega list

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from source import mega_list
 
 load_dotenv(".env")
 token = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+mega_types = mega_list.get_mega_list()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
---
+++ source/mega_list.py
@@ -0,0 +1,82 @@
+import requests
+
+
+def get_mega_list():
+    mega = [
+        "venusaur-mega",
+        "charizard-mega-x",
+        "charizard-mega-y",
+        "blastoise-mega",
+        "alakazam-mega",
+        "gengar-mega",
+        "kangaskhan-mega",
+        "gyarados-mega",
+        "pinsir-mega",
+        "aerodactyl-mega",
+        "mewtwo-mega-x",
+        "mewtwo-mega-y",
+        "ampharos-mega",
+        "scizor-mega",
+        "heracross-mega",
+        "houndoom-mega",
+        "tyranitar-mega",
+        "blaziken-mega",
+        "gardevoir-mega",
+        "mawile-mega",
+        "aggron-mega",
+        "medicham-mega",
+        "manectric-mega",
+        "banette-mega",
+        "absol-mega",
+        "latias-mega",
+        "latios-mega",
+        "garchomp-mega",
+        "lucario-mega",
+        "abomasnow-mega",
+        "beedrill-mega",
+        "pidgeot-mega",
+        "slowbro-mega",
+        "steelix-mega",
+        "sceptile-mega",
+        "swampert-mega",
+        "sableye-mega",
+        "sharpedo-mega",
+        "camerupt-mega",
+        "altaria-mega",
+        "glalie-mega",
+        "salamence-mega",
+        "metagross-mega",
+        "rayquaza-mega",
+        "lopunny-mega",
+        "gallade-mega",
+        "audino-mega",
+        "diancie-mega",
+    ]
+    primal_types = {
+        "primal groudon": ["grass", "fire", "ground"],
+        "primal kyogre": ["water", "bug", "electric"]
+    }
+
+    mega_types = {}
+    for m in mega:
+        mega_type = []
+        pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon/{m}").json()
+        mega_name = pokemon["name"].split("-")
+        if len(mega_name) == 2:
+            mega_name = " ".join(pokemon["name"].split("-")[::-1])
+        else:
+            mega_name = pokemon["name"].split("-")[::-1]
+            temp = mega_name.pop(0)
+            mega_name.append(temp)
+            mega_name = " ".join(mega_name)
+
+        if len(pokemon["types"]) == 1:
+            mega_type.append(pokemon["types"][0]["type"]["name"])
+        else:
+            mega_type.append(pokemon["types"][0]["type"]["name"])
+            mega_type.append(pokemon["types"][1]["type"]["name"])
+
+        mega_types[mega_name] = mega_type
+
+    mega_types.update(primal_types)
+    return mega_types
